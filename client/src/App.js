f9be6f8af0d9458ec35f8a9ebab82e0b13b55600
axgel <liangel421@gmail.com>
2023-03-31 02:45:08 -0400

    Init frontend files struct

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
-import './App.css';
+import './App.css'
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { GlobalStoreContextProvider } from './store'
+import { HomeScreen, RegisterScreen, SplashScreen } from './components'
+
 
 function App() {
   return (
-    <div>
-      <p>APP SCREEN</p>
-    </div>
-  );
+		<BrowserRouter>
+			<GlobalStoreContextProvider>
+				<Routes>
+					<Route path="/" element = {<SplashScreen/>} />
+					<Route path="/register" element = {<RegisterScreen/>} />
+					<Route path="/home" element = {<HomeScreen/>} />
+				</Routes>
+			</GlobalStoreContextProvider>
+		</BrowserRouter>
+	)
 }
 
 export default App;
