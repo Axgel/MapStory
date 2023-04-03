7ebe251221a6b8cc67b05c376065a87f89d33d9f
axgel <liangel421@gmail.com>
2023-04-03 04:14:37 -0400

    ci/cd cypress tests

--- client/cypress.config.js
+++ client/cypress.config.js
@@ -1,10 +1,16 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  fixturesFolder: '../tests/client_tests/cypress/fixtures',
+  video: false,
   e2e: {
     baseUrl: 'https://map-story-beta.vercel.app/',
+    supportFile: '../tests/client_tests/cypress/support/e2e.js',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+    specPattern: [
+      '../tests/client_tests/cypress/e2e/SplashScreen.cy.js'
+    ]
   },
 });
