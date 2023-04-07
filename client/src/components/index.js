e8ca9ed243121671e44d45c41777f6c8d14dff3a
axgel <liangel421@gmail.com>
2023-04-07 14:13:02 -0400

    Navbar frontend

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,13 +1,19 @@
 import MenuBar from './MenuBar';
 import SplashScreenButtons from './SplashScreenButtons';
 import Header from './Header';
-// import NavBar fsom './NavBar';
+import NavBar from './NavBar';
 import Map from './Map';
+import ViewModeButtons from './ViewModeButtons';
+import SortBy from './SortBy';
+import SearchBy from './SearchBy';
 
 export {
   MenuBar,
   SplashScreenButtons, 
   Header, 
-  // NavBar, 
-  Map
+  NavBar, 
+  Map,
+  ViewModeButtons,
+  SortBy,
+  SearchBy
 }
