2ce21778c0a09a72bfc73bbc0f5d2ee2b5bade1e
trxcyho <tracy.ho1107@gmail.com>
2023-04-17 03:13:25 -0400

    logout fet

---
+++ tests/client_tests/cypress/e2e/Account_management/Logout.cy.js
@@ -0,0 +1,14 @@
+describe('Logout', () => {
+    //success
+    it('logout: success', () => {
+        cy.login("test", "test")
+        cy.visit('/') 
+        cy.contains('Search By:')
+        cy.contains('Sort By:')
+        cy.get('#profileIcon').click()
+        cy.contains('Logout').click()
+        cy.url().should('include', '/')
+        cy.get('#loginEmail').should('exist')
+    });
+
+})
