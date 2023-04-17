ef66ad78179951083ec138c614abf3c1634f38a9
axgel <liangel421@gmail.com>
2023-04-17 01:33:59 -0400

    test server update

--- server/routes/store-router.js
+++ server/routes/store-router.js
@@ -3,6 +3,7 @@ const StoreController = require("../controllers/store-controller");
 const router = express.Router();
 const auth = require("../auth");
 
+// test
 router.post('/subregion', auth.verify, StoreController.createSubregion);
 router.post('/map', auth.verify, StoreController.createMap);
 router.get('/ownermaps/:userId', StoreController.getPersonalAndSharedMaps);
