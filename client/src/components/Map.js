d51821aba73dd069342493f76f8bb029b575f661
axgel <liangel421@gmail.com>
2023-04-20 01:09:19 -0400

    basic select subregion to edit

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,16 +1,19 @@
 import React, { useEffect, useState, useContext } from "react";
 import * as L from 'leaflet'
 import 'leaflet/dist/leaflet.css';
+import '@geoman-io/leaflet-geoman-free';  
+import '@geoman-io/leaflet-geoman-free/dist/leaflet-geoman.css';  
 import usastates from '../data/usastates.json'
 import { GlobalStoreContext } from '../store'
 import GlobalFileContext from "../file";
+import { EditMode } from "../enums";
 
 export default function Map() {
   const { store } = useContext(GlobalStoreContext);
   const { file } = useContext(GlobalFileContext);
   const [mapRef, setMapRef] = useState("");
   const [mapItem, setMapItem] = useState(null);
-  
+  const [tmpRegion, setTmpRegion] = useState(null);
 
   // Initializes leaflet map reference
   useEffect(()=> {
@@ -27,21 +30,69 @@ export default function Map() {
   // Load all subregions into map
   useEffect(()=>{
     if(!mapItem || !file.subregions) return;
-
+    if(file.loadedRegionOnce) return;
     // remove all preexisting layers
     mapItem.eachLayer(function (layer) {
       mapItem.removeLayer(layer);
     });
-
+    
+    let tmp = false;
     // Add all new subregion layers
     for(const region of file.subregions){
       const poly = L.polygon(region.coordinates).addTo(mapItem);
-      poly.on('click', function(e){
-        console.log(e);
-      })
+      poly.on('click', selectRegion)
+      tmp = true;
     }
+
+    if(tmp){
+      file.setLoadedRegionOnce();
+    }
+
   }, [mapItem, file])
 
+  useEffect(() => {
+    if(!tmpRegion || !file || !tmpRegion) return;
+
+    console.log(tmpRegion);
+    if(file.editRegions.includes(tmpRegion)){
+      tmpRegion.setStyle({ fillColor: '#3387FF'});
+      tmpRegion.pm.disable();
+      file.updateEditRegions(file.editRegions.filter(item => item != tmpRegion));
+    }
+    else {
+      file.updateEditRegions([...file.editRegions, tmpRegion]);
+    }
+    
+    console.log(tmpRegion == file.editRegions[0]);
+    setTmpRegion(null);
+  }, [tmpRegion])
+
+  useEffect(() => {
+    for(const region of file.editRegions){
+      region.setStyle({ fillColor: 'red'});
+      region.pm.disable();
+      if(file.currentEditMode == EditMode.EDIT_VERTEX){
+        region.pm.enable({
+          limitMarkersToCount: 10,
+          draggable: false,
+          // removeVertexOn: 'click',
+          // removeVertexValidation: store.removeVertexValidate,
+          addVertexOn: 'click',
+          addVertexValidation: addVertexValidate
+        }) 
+      }
+    }
+  }, [file])
+
+
+  function addVertexValidate(e){
+    console.log(e.event.latlng);
+    return false;
+  }
+
+  function selectRegion(e){
+    setTmpRegion(e.target);
+  }
 
   // get div of screen on page load to add map to
   function handleInitMapLoad(e){
