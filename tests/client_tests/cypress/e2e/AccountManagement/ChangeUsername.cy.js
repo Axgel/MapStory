95ad58d7a2ce6ad00e92218a4f3e717a6ebe2329
trxcyho <tracy.ho1107@gmail.com>
2023-04-22 17:11:45 -0400

    test original change username and password

--- tests/client_tests/cypress/e2e/AccountManagement/ChangeUsername.cy.js
+++ tests/client_tests/cypress/e2e/AccountManagement/ChangeUsername.cy.js
@@ -1,3 +1,66 @@
+// describe('Change Username', () => {
+//     beforeEach(() => {
+//         cy.login("test", "test")
+//         cy.visit('/')
+//         cy.get('#profileIcon').click()
+//         cy.contains('Profile').click()
+//     });
+//     //press cancel on modal
+//     it('cancel', () =>{
+//         cy.get('#profileUsername').then((currentUsername)=>{
+//             cy.wait(1000)
+//             cy.get('#editUsernameIcon').click() //click edit icon
+//             cy.get('#inputNewUsername').clear().type('cancelcancel') 
+//             cy.contains('Cancel').click()
+//             //check that the username field has the same value as previous
+//             cy.get('#profileUsername').should((newUsername) => {
+//                 expect(newUsername[0]).to.eql(currentUsername[0]);
+//             })
+//         })
+//     });
+//     // username not unique
+//     it('nonunique username', () =>{
+//         cy.get('#profileUsername').then((currentUsername)=>{
+//             cy.wait(1000)
+//             cy.get('#editUsernameIcon').click() //click edit icon
+//             cy.get('#inputNewUsername').clear().type('tray') //TODO
+//             cy.contains('OK').click()
+//             //check error
+//             cy.contains('An account with this username already exists.')
+//             cy.contains('OK').click()
+//             //check that the username field has the same value as previous
+//             cy.get('#profileUsername').should((newUsername) => {
+//                 expect(newUsername[0]).to.eql(currentUsername[0]);
+//             })
+//         })
+//     });
+//     //empty username
+//     it('empty username', () =>{
+//         cy.get('#profileUsername').then((currentUsername)=>{
+//             cy.wait(1000)
+//             cy.get('#editUsernameIcon').click() //click edit icon
+//             cy.get('#inputNewUsername').clear() //TODO
+//             cy.contains('OK').click()
+//             //check error
+//             cy.contains('Username cannot be empty')
+//             cy.contains('Cancel').click()
+//             //check that the username field has the same value as previous
+//             cy.get('#profileUsername').should((newUsername) => {
+//                 expect(newUsername[0]).to.eql(currentUsername[0]);
+//             })
+//         })
+//     });
+//     //success
+//     it('success', () =>{
+//         cy.get('#editUsernameIcon').click()
+//         cy.wait(1000);
+//         cy.get('#inputNewUsername').clear().type("newUsername") 
+//         cy.intercept('POST', '/auth/profile/username', {})
+//         cy.contains('OK').click()
+//     });
+    
+// })
+
 describe('Change Username', () => {
     beforeEach(() => {
         cy.login("test", "test")
@@ -55,7 +118,12 @@ describe('Change Username', () => {
         cy.get('#editUsernameIcon').click()
         cy.wait(1000);
         cy.get('#inputNewUsername').clear().type("newUsername") 
-        cy.intercept('POST', '/auth/profile/username', {})
+        cy.contains('OK').click()
+        //check that the username field has the new value
+        cy.get('#profileUsername').should('have.value', 'newUsername')
+        //reset to old username
+        cy.get('#editUsernameIcon').click()
+        cy.get('#inputNewUsername').clear().type("fet") 
         cy.contains('OK').click()
     });
     
