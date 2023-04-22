243593aa09cea9d4f5a99f1352db4f94f521cd10
trxcyho <tracy.ho1107@gmail.com>
2023-04-21 23:53:11 -0400

    remove tag testing

--- client/src/components/TagCard.js
+++ client/src/components/TagCard.js
@@ -16,7 +16,7 @@ export default function TagCard(props) {
     <div className="flex items-center gap-5">
       <div className="w-auto h-[25px] rounded-lg shadow-lg bg-white outline-none border-none px-2 text-lg flex justify-between items-center gap-3">
         <p id="displayed_tag" className="text-sm">{props.tag}</p>
-        <img className="w-[15px] h-[15px]" src={closeIcon} alt="" onClick={handleDeleteTags}></img>
+        <img id="removeTagBtn" className="w-[15px] h-[15px]" src={closeIcon} alt="" onClick={handleDeleteTags}></img>
       </div>
     </div>
 
---
+++ tests/client_tests/cypress/e2e/MapIdentification/RemoveTag.cy.js
@@ -0,0 +1,25 @@
+describe('Remove Tag', () => {
+    beforeEach(() => {
+        cy.login("test", "test")
+        cy.visit('/')
+        cy.get('#mapsid').should('exist'); //verify on home page
+        //double click DO NOT DELETE (map name)
+        cy.contains("TESTING").dblclick()
+        cy.url().should('include', '/map')
+        cy.get('#tagsBtn').click() //click on tag
+        cy.contains("Add Tags") 
+    });
+    
+    //tag already exists
+    
+    //success
+    it('success', () => {
+        // cy.get(':nth-child(1) > .w-auto > #displayed_tag').exists()
+        cy.get(':nth-child(1) > .w-auto > #displayed_tag').then((tag)=>{
+            cy.get(':nth-child(1) > .w-auto > #removeTagBtn').click() //click X icon
+            //check that the username field has the same value as previous
+            cy.get(':nth-child(1) > .w-auto > #displayed_tag').should("not.contain", tag)
+        })
+    });
+
+})
