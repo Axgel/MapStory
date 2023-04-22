ced9bcadd2faf980e4e2601d775608da64fec23d
trxcyho <tracy.ho1107@gmail.com>
2023-04-21 23:00:26 -0400

    rerun with old import map code

--- tests/client_tests/cypress/e2e/MapServices/ImportMap.cy.js
+++ tests/client_tests/cypress/e2e/MapServices/ImportMap.cy.js
@@ -7,8 +7,21 @@ describe('Import Map', () => {
         cy.contains("Upload File")
     });
     //file not formatted properly (SHP + JSON)
+    
     //success
     it('success', () => {
+        // cy.contains("Sort By:")
+        // cy.contains("Search By:")
+        // cy.intercept("GET", '/store/publishedmaps').as('getPublishedMaps')
+        
+        // cy.wait('@getPublishedMaps').its('response.statusCode').should('eq', 200)
+        // cy.wait('@getPublishedMaps').then((response) => {
+        //     cy.contains("Upload File")
+        //     cy.get('#fileUpload').click()
+        // })
+        // cy.wait(5000)
+        // cy.wait('@getPublishedMaps')
+        
         cy.get('#fileUpload').click()
         cy.get('#fileUpload').selectFile('./cypress/fixtures/usastates.json')
         cy.get('#uploadFileBtn').click()
@@ -17,3 +30,23 @@ describe('Import Map', () => {
     });
 
 })
+
+// describe('Import Map', () => {
+//     beforeEach(() => {
+//         cy.login("test", "test")
+//         cy.visit('/')
+//         cy.get('#mapsid').should('exist'); //verify on home page
+//         cy.get('#createMapBtn').click() //click create map
+//         cy.contains("Upload File")
+//     });
+//     //file not formatted properly (SHP + JSON)
+//     //success
+//     it('success', () => {
+//         cy.get('#fileUpload').click()
+//         cy.get('#fileUpload').selectFile('./cypress/fixtures/usastates.json')
+//         cy.get('#uploadFileBtn').click()
+//         cy.wait(1000)
+//         cy.url().should('include', '/map')
+//     });
+
+// })
