7c3cecd6f2661c50a3c6be22822164166f70b2ad
trxcyho <tracy.ho1107@gmail.com>
2023-04-23 01:45:19 -0400

    testing username wait

--- tests/client_tests/cypress/e2e/AccountManagement/ChangeUsername.cy.js
+++ tests/client_tests/cypress/e2e/AccountManagement/ChangeUsername.cy.js
@@ -2,13 +2,13 @@ describe('Change Username', () => {
     beforeEach(() => {
         cy.login("test", "test")
         cy.visit('/')
+        cy.wait(1000)
         cy.get('#profileIcon').click()
         cy.contains('Profile').click()
     });
     //press cancel on modal
     it('cancel', () =>{
         cy.get('#profileUsername').then((currentUsername)=>{
-            cy.wait(1000)
             cy.get('#editUsernameIcon').click() //click edit icon
             cy.get('#inputNewUsername').clear().type('cancelcancel') 
             cy.contains('Cancel').click()
@@ -21,7 +21,7 @@ describe('Change Username', () => {
     // username not unique
     it('nonunique username', () =>{
         cy.get('#profileUsername').then((currentUsername)=>{
-            cy.wait(1000)
+            // cy.wait(1000)
             cy.get('#editUsernameIcon').click() //click edit icon
             cy.get('#inputNewUsername').clear().type('tray') //TODO
             cy.contains('OK').click()
@@ -37,7 +37,7 @@ describe('Change Username', () => {
     //empty username
     it('empty username', () =>{
         cy.get('#profileUsername').then((currentUsername)=>{
-            cy.wait(1000)
+            // cy.wait(1000)
             cy.get('#editUsernameIcon').click() //click edit icon
             cy.get('#inputNewUsername').clear() //TODO
             cy.contains('OK').click()
@@ -53,7 +53,7 @@ describe('Change Username', () => {
     //success
     it('success', () =>{
         cy.get('#editUsernameIcon').click()
-        cy.wait(1000);
+        // cy.wait(1000);
         cy.get('#inputNewUsername').clear().type("newUsername") 
         cy.intercept('POST', '/auth/profile/username', {})
         cy.contains('OK').click()
