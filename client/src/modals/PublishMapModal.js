2874f912ae2fa76e2b0d724944465d9875117c46
trxcyho <tracy.ho1107@gmail.com>
2023-04-26 20:49:00 -0400

    update publishing map modal

--- client/src/modals/PublishMapModal.js
+++ client/src/modals/PublishMapModal.js
@@ -8,6 +8,13 @@ export default function PublishMapModal(props) {
   const { store } = useContext(GlobalStoreContext);
   const { auth } = useContext(AuthContext);
 
+  const mapInfo = store.selectedMap;
+
+  const collaboratorsUsernames = [];
+  store.collaborators.forEach(collaborator => {
+    collaboratorsUsernames.push(collaborator.userName)
+  }); 
+
   function handleCloseModal(e){
     e.stopPropagation();
     store.setCurrentModal(CurrentModal.NONE);
@@ -22,16 +29,17 @@ export default function PublishMapModal(props) {
     return (
       <div className="fixed inset-x-0 inset-y-0 flex items-center justify-center z-50">
         <div className="bg-brownshade-700 border-modalborder border-solid border rounded-lg w-96 text-center">
-          <h1 className="text-xl mt-4 mb-4 mx-9">{props.title}</h1>
+          <h1 className="text-xl mt-4 mb-4 mx-9">Publishing Map</h1>
           <h1 className="">Map Name</h1>
-          <p className="mb-2 text-publishmodalsubtext">{props.mapTitle}</p>
+          <p className="mb-2 text-publishmodalsubtext">{mapInfo.title}</p>
           <h1 className="">Owner</h1>
-          <p className="mb-2 text-publishmodalsubtext">{props.owner}</p>
+          <p className="mb-2 text-publishmodalsubtext">{mapInfo.ownerName}</p>
           <h1 className="">Collaborators</h1>
-          <p className="mb-2 text-publishmodalsubtext">{props.collaborators}</p>
+          <p className="mb-2 text-publishmodalsubtext">{(mapInfo.collaborators.length === 0) ? "There are no collaborators for this map" : collaboratorsUsernames.join(", ")}</p>
           <h1 className="">Tags</h1>
-          <p className="mb-4 text-publishmodalsubtext">{props.tags}</p>
-          <h1 className="mx-6 my-2 "> {props.message} </h1>
+          <p className="mb-4 text-publishmodalsubtext">{(mapInfo.tags.length === 0) ? "There are currently no tags for this map" : mapInfo.tags.join(", ")}</p>
+          <h1 className="mx-6 my-2 ">Are you sure you want to publish '{mapInfo.title}'. This action cannot be undone.</h1>
+          <h1 className="mx-6 my-2 ">*You will no longer be able to edit this map.</h1>
           <div className="flex flex-row-reverse">
             <button className="bg-brownshade-800 text-white mb-3 mr-3 px-3 rounded-md border-brownshade-850" onClick={handlePublishMap}>OK</button>
             <button className="bg-brownshade-800 text-white mb-3 mr-3 px-3 rounded-md border-brownshade-850" onClick={handleCloseModal}>Cancel</button>
