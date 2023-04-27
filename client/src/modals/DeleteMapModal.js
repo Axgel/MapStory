5246e062d8c2bf044edfae9ff401f5f0a2d31746
trxcyho <tracy.ho1107@gmail.com>
2023-04-26 20:51:30 -0400

    update delete map modal

--- client/src/modals/DeleteMapModal.js
+++ client/src/modals/DeleteMapModal.js
@@ -8,6 +8,8 @@ export default function DeleteMapModal(props) {
   const { store } = useContext(GlobalStoreContext);
   const { auth } = useContext(AuthContext);
 
+  const mapInfo = store.selectedMap;
+
   function handleCloseModal(e){
     e.stopPropagation();
     store.setCurrentModal(CurrentModal.NONE);
@@ -24,9 +26,9 @@ export default function DeleteMapModal(props) {
 
         <div className="bg-brownshade-700 border-modalborder border-solid border rounded-lg w-96 text-right">
           <h1 className="text-xl text-center mt-4 mb-2 mx-9">Deleting Map</h1>
-          <h1 className="text-xl text-center mt-4 mb-2 mx-9">{props.title}</h1>
-          <h1 className="mx-6 my-2 text-center"> {props.message} </h1>
-          <h1 className="mx-6 my-2 text-center"> This map will be deleted. Action cannot be undone</h1><br></br><br></br>
+          <h1 className="text-xl text-center mt-4 mb-2 mx-9">{mapInfo.title}</h1>
+          <h1 className="mx-6 my-2 text-center"> Are you sure you want to permanently delete '{mapInfo.title}'? </h1>
+          <h1 className="mx-6 my-2 text-center"> *This action cannot be undone.</h1><br></br><br></br>
           <div className="flex flex-row-reverse mx-3">
             <button className="bg-brownshade-800 text-white mb-3 mr-3 px-3 rounded-md border-brownshade-850" onClick={handleDeleteMap}>OK</button>
             <button className="bg-brownshade-800 text-white mb-3 mr-3 px-3 rounded-md border-brownshade-850" onClick={handleCloseModal}>Cancel</button>
