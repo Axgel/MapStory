9dbf7dd71aef4425e057fbfdf75e7651616c2dff
axgel <liangel421@gmail.com>
2023-04-28 05:29:07 -0400

    Added console logs

--- client/src/file/index.js
+++ client/src/file/index.js
@@ -71,7 +71,7 @@ function GlobalFileContextProvider(props) {
       (so maybe removed items in the queue only up to server version)
     */
     auth.socket.on('owner-ack', (data) => {
-      console.log(data.serverVersion);
+      console.log(`owner: ${data.serverVersion}`);
       file.incrementVersion();
       setQueue(queue.slice(1));
     })
@@ -79,7 +79,7 @@ function GlobalFileContextProvider(props) {
     /* STEP 6: All other users receive the change */
     auth.socket.on('others-ack', (data) => {
       const {serverVersion, op} = data
-      console.log(serverVersion);
+      console.log(`others: ${serverVersion}`);
       if(!queue.length) {
         file.incrementVersionAndUpdateSubregions(op);
       } else {
