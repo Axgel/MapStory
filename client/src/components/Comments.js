89921e1b85eed787b72c281e2d2742a72cfb5979
trxcyho <tracy.ho1107@gmail.com>
2023-04-30 01:03:59 -0400

    front end for comments

--- client/src/components/Comments.js
+++ client/src/components/Comments.js
@@ -1,6 +1,12 @@
 import React from "react";
+import paperPlaneIcon from "../assets/paperPlaneIcon.png"
 
 export default function Comments() {
+
+  function handleComment(e){
+    //add comment to database
+  }
+
   return (
     <div className="flex flex-col pl-5 pt-4 gap-2">
       <div>
@@ -16,6 +22,10 @@ export default function Comments() {
       <div>
         <p className="font-semibold text-base">janeD43</p>
         <p className="text-sm text-publishmodalsubtext">I plan on making an extension of this app soon!</p>
+      </div>
+      <div className="absolute bottom-2 left-3">
+        <input className="w-[230px] h-[35px] rounded-lg shadow-lg bg-white outline-none border-none pl-2 text-base" type="text"></input>
+        <img className="h-[35px] absolute bottom-0" src={paperPlaneIcon} onClick={handleComment} alt=""></img>
       </div>
     </div>
   );
