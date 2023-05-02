e6016bab3ce393df64747e65bc02fe5de6fb745a
trxcyho <tracy.ho1107@gmail.com>
2023-05-01 21:03:36 -0400

    make comments scrollable

--- client/src/components/Comments.js
+++ client/src/components/Comments.js
@@ -33,17 +33,8 @@ export default function Comments(props) {
   }
 
   return (
-    <div id="commentView" className="flex flex-col pl-5 pt-4 gap-2">
-      {commentCard}
-      {/* <div>
-        <p className="font-semibold text-base">janeD43</p>
-        <p className="text-sm text-publishmodalsubtext">This is such a useful map for my history class!</p>
-      </div>
-
-      <div>
-        <p className="font-semibold text-base">janeD43</p>
-        <p className="text-sm text-publishmodalsubtext">I plan on making an extension of this app soon!</p>
-      </div> */}
+    <div id="commentView" className="flex flex-col pl-5 pr-2 py-3  gap-2">
+      <div className="overflow-auto h-[400px] mb-2">{commentCard}</div>
       <div className="absolute bottom-2 left-3">
         <input id="input_comment" onKeyDown={handleKeyPress} className="w-[230px] h-[35px] rounded-lg shadow-lg bg-white outline-none border-none pl-2 text-base" type="text"></input>
         <img id="submit_comment" className="h-[35px] absolute bottom-0" src={paperPlaneIcon} onClick={handleComment} alt=""></img>
